04a98698a5d13c7038b1c06311cf52e4ea9c90e8
master <master@pop-os.localdomain>
2022-04-05 22:37:47 +0530

    Dealy of Day Fix

--- src/components/ads/amazon/dealofday.js
+++ src/components/ads/amazon/dealofday.js
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import { product } from "./Deals.module.css";
-import { BlogPageAds } from "./adsLink";
+import firebase from "../../../firebase";
 
 export default function DealOfDay() {
+  const [loading, setLoading] = useState(false);
+  const [offers, setOffers] = useState([]);
+  useEffect(() => {
+    const getOffers = () => {
+      const ref = firebase
+        .firestore()
+        .collection("offers")
+        .orderBy("date", "desc")
+        .limit(4);
+      setLoading(true);
+      ref.onSnapshot((querySnapshot) => {
+        const items = [];
+        querySnapshot.forEach((doc) => {
+          items.push({ id: doc.id, ...doc.data() });
+        });
+        setOffers(items);
+        setLoading(false);
+      });
+    };
+    getOffers();
+  }, []);
+
+  console.log(offers);
+
   return (
     <div>
       <div>
@@ -11,16 +36,33 @@ export default function DealOfDay() {
         </p>
       </div>
       <div className="d-flex ">
-        {BlogPageAds.map((ads, index) => (
-          <div className={product}>
-            <a href={ads.link}>
-              <img src={ads.img} width="50%" alt="amazon deal" />
-            </a>
-            <p style={{ fontSize: 13 }}>
-              <u>{ads.title}</u>
-            </p>
-          </div>
-        ))}
+        {loading === true ? (
+          <div />
+        ) : (
+          offers.map((ads, index) => (
+            <div className="">
+              <div className={product}>
+                <a href={ads.link} className="link-secondary">
+                  <div className="d-flex justify-content-center">
+                    <img
+                      className="d-flex align-items-center"
+                      src={ads.img}
+                      width="100px"
+                      height="100px"
+                      alt="amazon deal"
+                    />
+                  </div>
+                  <p
+                    className="d-none d-sm-block text-center"
+                    style={{ fontSize: 16 }}
+                  >
+                    <u>{ads.title}</u>
+                  </p>
+                </a>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
--- src/components/ads/amazon/homeSide.js
+++ src/components/ads/amazon/homeSide.js
@@ -1,21 +1,56 @@
 import * as React from "react";
-import { HomePageAds } from "./adsLink";
+import { useEffect, useState } from "react";
+import { product } from "./Deals.module.css";
+import firebase from "../../../firebase";
 
 export function AmazonHomePageAds() {
+  const [loading, setLoading] = useState(false);
+  const [offers, setOffers] = useState([]);
+  useEffect(() => {
+    const getOffers = () => {
+      const ref = firebase
+        .firestore()
+        .collection("offers")
+        .orderBy("date", "desc")
+        .limit(4);
+      setLoading(true);
+      ref.onSnapshot((querySnapshot) => {
+        const items = [];
+        querySnapshot.forEach((doc) => {
+          items.push({ id: doc.id, ...doc.data() });
+        });
+        setOffers(items);
+        setLoading(false);
+      });
+    };
+    getOffers();
+  }, []);
   return (
     <div>
-      {HomePageAds.map((ads, index) => (
-        <div className="row mt-1 pt-2">
-          <div className="col-md-6 ">
-            <img src={ads.img} width="100%" />
+      {loading === true ? (
+        <div />
+      ) : (
+        offers.map((ads, index) => (
+          <div className="d-none d-sm-block">
+            <div className={product}>
+              <div className="row mt-1 pt-2">
+                <div className="col-md-6 ">
+                  <div className="d-flex justify-content-center">
+                    <img src={ads.img} width="100px" height="100px" />
+                  </div>
+                </div>
+                <div className="col-md-6 mt-sm-2 mt-md-0">
+                  <div className="text-center">
+                    <a href={ads.link} style={{ color: "black" }}>
+                      <p> {ads.title}</p>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-md-6 mt-sm-2 mt-md-0">
-            <a href={ads.link} style={{ color: "black" }}>
-              <p> {ads.title}</p>
-            </a>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 }

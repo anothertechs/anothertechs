facf270b621fa21479aca05749ffffc813d83e21
Master52 <harajmast9008@gmail.com>
2022-11-02 21:57:06 +0530

    googel tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,30 @@ module.exports = {
         policy: [{ userAgent: "*", allow: "/" }],
       },
     },
+       {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-WY6TT5EQVC", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Defaults to https://www.googletagmanager.com
+          //origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
+      },
+    },
     "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-advanced-sitemap`,
